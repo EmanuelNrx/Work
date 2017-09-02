9e8a3b4c20d6eab6d0469e536e0c6ad7833217e7
EmanuelNrx <emanuelnrx@gmail.com>
2017-09-02 18:00:55 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+    © by EmanuelNrx
+ 
+    This problem is easy, everyone knows that :)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ifstream input("adunare.in");
+ofstream output("adunare.out");
+
+int main(void)
+{
+    int a, b;
+    
+    input >> a >> b;
+    output << a + b << endl;
+    
+    return 0;
+}
